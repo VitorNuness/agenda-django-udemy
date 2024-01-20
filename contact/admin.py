8b9f6da509bcac5b5a209ce353961a2ae5988d69
Vitor Nunes <vitor1nuness@gmail.com>
2024-01-20 13:17:48 -0300

    configurando exibição

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,8 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 20
+    list_max_show_all = 200

d80d153f917016648b96cc944be6e1c18e5ebdb0
Vitor Nunes <vitor1nuness@gmail.com>
2024-01-23 06:31:59 -0300

    paginação criada

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.db.models import Q
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos'
         
     }
@@ -33,8 +38,12 @@ def search(request):
                                   )\
                               .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search',
         'seach_value': search_value
     }
